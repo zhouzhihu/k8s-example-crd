9af59499d2be5e3b21b27016044bb969d7f26300
zhouzhihu <13560107927@139.com>
2021-05-06 17:17:36 +0800

    fix type error

--- pkg/apis/example/v1beta1/canary.go
+++ pkg/apis/example/v1beta1/canary.go
@@ -10,25 +10,25 @@ import (
 
 // Canary is the configuration for a canary release
 type Canary struct {
-	metav1.TypeMeta   	`json:",inline"`
-	metav1.ObjectMeta 	`json:"metadata,omitempty"`
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   CanarySpec   `json:"spec"`
+	Spec CanarySpec `json:"spec"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
 // CanaryList is a list of Canary resources
 type CanaryList struct {
-	metav1.TypeMeta 	`json:",inline"`
-	metav1.ListMeta 	`json:"metadata"`
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
 
-	Items []Canary 		`json:"items"`
+	Items []Canary `json:"items"`
 }
 
 // CanarySpec is the specification of the desired behavior of the Canary
 type CanarySpec struct {
-	Image		string	`json:"image,omitempty"`
-	Cron		string	`json:"cron"`
-	Replicas	string	`json:"replicas"`
+	Image    string `json:"image,omitempty"`
+	Cron     string `json:"cron"`
+	Replicas int32  `json:"replicas"`
 }
